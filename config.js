e126868475bddf86f1eddc4839c8c2f0b87a0886
to nguyen duc manh <ducmanh14032000@gmail.com>
2023-11-30 06:11:52 +0700

    giới hạn limitLine xuống còng 70 1 lần call chatGPT

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ export default {
   specialKeyPath: "./output/specialKey.js", // file js lưu object các ký tự đặc biệt không dịch
   // danh sách ký tự cần replace, sử dụng biểu thức chính quy, replace các cặp {} và ""
   specialKeyNeedReplace: [/\{(.*?)\}/gi, /\"(.*?)\"/gi],
-  limitLine: 100, // số key giới hạn dịch 1 lần gọi tới chat GPT
+  limitLine: 70, // số key giới hạn dịch 1 lần gọi tới chat GPT
   modelGPT: "gpt-3.5-turbo", // model chat gpt sẽ sử dụng
   roleCallGPT: "user",
   splitResultChar: ";;;;",
